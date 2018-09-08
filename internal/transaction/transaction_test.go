88e39af01b3d60c79f55496a24970e554fe9325b
Arash Bina <arash@arash.io>
2018-09-08 05:05:13 -0400

    convert to table driven tests in transaction package

--- internal/transaction/transaction_test.go
+++ internal/transaction/transaction_test.go
@@ -7,58 +7,246 @@ import (
 
 func TestGetRawDataToSign(t *testing.T) {
 
-	input := TXInput{
-		PrevTxHash:  []byte("hash1"),
-		OutputIndex: 1,
-		Signature:   []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+	tests := []struct {
+		tx       Transaction
+		expected []byte
+		res      bool
+	}{
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 1,
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      true,
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 2,
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //outputIndex mismatch
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash2"),
+						OutputIndex: 1,
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //prevTxHash mismatch
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 1,
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.2,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //Value mismatch
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 1,
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey2"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //Address mismatch
+		},
 	}
 
-	output := TXOutput{
-		Value:   1.0,
-		Address: []byte("publicKey1"),
+	for i, test := range tests {
+		data, err := test.tx.GetRawDataToSign(0)
+		if data == nil || err != nil {
+			t.Errorf("test: %d\n error getting data to sign: %s", i, err)
+		}
+
+		if cmp := bytes.Compare(test.expected, data); (cmp == 0) != test.res {
+			t.Errorf("test: %d\n signature mismatch. Expected: %q, Got: %q", i, test.expected, data)
+		}
 	}
 
-	tx := Transaction{
-		Inputs:  []TXInput{input},
-		Outputs: []TXOutput{output},
-	}
-
-	data, err := tx.GetRawDataToSign(0)
-	if data == nil || err != nil {
-		t.Errorf("error getting data to sign: %s", err)
-	}
-
-	if cmp := bytes.Compare(input.Signature, data); cmp != 0 {
-		t.Errorf("signature mismatch. Expected: %q, Got: %q", input.Signature, data)
-	}
 }
 
 func TestGetRawTransaction(t *testing.T) {
-
-	input := TXInput{
-		PrevTxHash:  []byte("hash1"),
-		OutputIndex: 1,
-		Signature:   []byte("signature1"),
+	tests := []struct {
+		tx       Transaction
+		expected []byte
+		res      bool
+	}{
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 1,
+						Signature:   []byte("signature1"),
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00signature1\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      true,
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash2"),
+						OutputIndex: 1,
+						Signature:   []byte("signature1"),
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00signature1\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //hash mismatch
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 2,
+						Signature:   []byte("signature1"),
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00signature1\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //outputIndex mismatch
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 1,
+						Signature:   []byte("signature2"),
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00signature1\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //signature mismatch
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 1,
+						Signature:   []byte("signature1"),
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.2,
+						Address: []byte("publicKey1"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00signature1\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //value mismatch
+		},
+		{
+			tx: Transaction{
+				Inputs: []TXInput{
+					{
+						PrevTxHash:  []byte("hash1"),
+						OutputIndex: 1,
+						Signature:   []byte("signature1"),
+					},
+				},
+				Outputs: []TXOutput{
+					{
+						Value:   1.0,
+						Address: []byte("publicKey2"),
+					},
+				},
+			},
+			expected: []byte("hash1\x01\x00\x00\x00\x00\x00\x00\x00signature1\x00\x00\x00\x00\x00\x00\xf0?publicKey1"),
+			res:      false, //address mismatch
+		},
 	}
 
-	output := TXOutput{
-		Value:   1.0,
-		Address: []byte("publicKey1"),
-	}
+	for i, test := range tests {
+		data, err := test.tx.GetRawTransaction()
+		if data == nil || err != nil {
+			t.Errorf("test: %d\n could not get transaction: %s", i, err)
+		}
 
-	tx := Transaction{
-		Inputs:  []TXInput{input},
-		Outputs: []TXOutput{output},
-	}
-
-	expected := "hash1\x01\x00\x00\x00\x00\x00\x00\x00signature1\x00\x00\x00\x00\x00\x00\xf0?publicKey1"
-
-	data, err := tx.GetRawTransaction()
-	if err != nil || data == nil {
-		t.Errorf("could not get transaction: %s", err)
-	}
-
-	if cmp := bytes.Compare([]byte(expected), data); cmp != 0 {
-		t.Errorf("raw bytes mismatch. Expected %q, Got: %q", expected, data)
+		if cmp := bytes.Compare(test.expected, data); (cmp == 0) != test.res {
+			t.Errorf("test: %d\n bytes mismatch. Expected: %q, Got: %q", i, test.expected, data)
+		}
 	}
 }
